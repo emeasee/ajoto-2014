3f169ee5b53755d0e3a10bf6f318ddbc5c7a0748
Mac Oosthuizen <Mac@MacBook-Air-de-Mac.local>
2014-01-10 13:45:45 +0100

    Fixed ipad scaling problems and changed prices on Pen product page

--- wp-content/themes/AJOTO/the-pen.php
+++ wp-content/themes/AJOTO/the-pen.php
@@ -39,7 +39,7 @@
 	
 	<section class="screen sans-serif">
 		<div class="card cube quote">
-			<a class="serif" href="../../shop/the-pen">Starting from £80
+			<a class="serif" href="../../shop/the-pen">Starting from £126
 			<span class="sans-serif">CLICK HERE TO SHOP</span></a>
 		</div>
 		<div class="card three grey">
@@ -74,7 +74,7 @@
 			</i></p>
 		</div>
 		<div class="card cube quote">
-			<a class="serif" href="../../shop/the-pen">Starting from £80
+			<a class="serif" href="../../shop/the-pen">Starting from £126
 			<span class="sans-serif">CLICK HERE TO SHOP</span></a>
 		</div>
 	</section>
