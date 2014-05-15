3b97420213444ae1d7df5c3cfe5406a8d54c3084
Mac Oosthuizen <hello@m-jo.co>
2014-05-15 13:35:34 +0200

    Updated products & fix CSS issues

--- wp-content/themes/AJOTO/shop.php
+++ wp-content/themes/AJOTO/shop.php
@@ -18,7 +18,7 @@
 					<p>THE PEN<span>WE WANTED TO DO JUSTICE TO THE MOST IMPORTANT TOOL WE USE EVERYDAY. THAT’S WHY WE CREATED A PEN THAT NOT ONLY HAS A GREAT STORY BUT MARkS THE BEGINNING OF A JOURNEY THAT WILL LAST A LIFETIME.<br><br>EACH PEN COMES WRAPPED IN AN ANODISED ALUMINIUM BOX WITH A MOULDED CORk TRAY.</span><a href="../products/the-pen" class="button">FIND OUT MORE</a></p>
 				</section>
 
-				<div id="pen"><?php echo do_shortcode( '[products skus="MYM001SP,MYM001RAWP,MYM001BP,MYM001BRP" orderby="date" order="desc"]' ); ?></div>
+				<div id="pen"><?php echo do_shortcode( '[products skus="MYM001RAWP,MYM001BP,MYM001SBP,MYM001BSP" orderby="date" order="desc"]' ); ?></div>
 
 				<section class="title mid subhead" id="refills">
 					<p>INK REFILLS<span>A PEN IS ONLY AS GOOD AS THE qUALITY OF THE INk IT USES. WE WORk WITH ONE OF THE WORLD’S FINEST REFILL MANUFACTURERS IN THE WORLD TO ENSURE OUR INk IS OF THE HIGHEST STANDARD.</span><a href="../products/the-pen" class="button">FIND OUT MORE</a></p>
